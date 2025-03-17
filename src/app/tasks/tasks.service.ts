e17ba6898966cfafa8b07f2259a074071d199239
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-03-17 15:34:43 +0100

    First Commit

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, OnInit, signal } from "@angular/core";
+import { Task, TaskStatus } from "./task.model";
+
+@Injectable(
+    {
+        providedIn: 'root'
+    }
+)
+export class TasksService implements OnInit {
+
+    private tasks = signal<Task[]>([]);
+
+    allTasks = this.tasks.asReadonly();
+
+    ngOnInit(): void {
+        console.log('TasksService');
+    }
+
+    constructor() {}
+    
+    addTask(taskdata:{title: string, description: string}) {
+        const newTask: Task = { ...taskdata, id: Math.random().toString(), status: 'OPEN' };
+        this.tasks.update((tasks) => [...tasks, newTask]);
+    }
+
+    updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+        this.tasks.update((oldTasks) => oldTasks.map(
+            (task)=>
+                task.id === taskId ? {...task, status: newStatus} : task
+        ) );  
+      }
+}
